c035788198a51ada6f707de4b67477829e8cd833
neumont-gamedev <rmaple@neumont.edu>
2022-01-11 12:26:47 -0700

    class 011122

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5;
+    [Range(0, 10)][Tooltip("speed of the player")] public float speed = 5;
+    [SerializeField] AudioSource audioSource;
 
-    void Update()
+
+	void Update()
     {
         Vector3 direction = Vector3.zero;
 
@@ -14,5 +16,20 @@ public class Player : MonoBehaviour
         direction.z = Input.GetAxis("Vertical");
 
         transform.position += direction * speed * Time.deltaTime;
+        //transform.rotation *= Quaternion.Euler(5, 0, 0);
+        //transform.localScale = new Vector3(2, 2, 2);
+
+        if (Input.GetButtonDown("Fire1"))
+		{
+            audioSource.Play();
+            GetComponent<Renderer>().material.color = Color.green;
+            //transform.rotation *= Quaternion.Euler(5, 0, 0);
+        }
+
+        GameObject go = GameObject.Find("Cube");
+        if (go)
+		{
+            go.GetComponent<Renderer>().material.color = Color.cyan;
+		}
     }
 }
