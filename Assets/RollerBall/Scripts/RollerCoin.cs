a01bc1e94148724c0a0965930072e359d975f7e1
neumont-gamedev <rmaple@neumont.edu>
2022-02-10 11:05:09 -0700

    class 020922

--- Assets/RollerBall/Scripts/RollerCoin.cs
+++ Assets/RollerBall/Scripts/RollerCoin.cs
@@ -2,35 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RollerCoin : MonoBehaviour, IDestructable
+public class RollerCoin : RollerPickup, IDestructable
 {
-    public float amplitude;
-    public float rate;
-    public float spinRate;
+    [SerializeField] int points;
 
-    Vector3 initialPosition;
-    float time;
-    float angle;
-
-	public void Destroyed()
-	{
-		RollerGameManager.Instance.Score++;
-	}
-
-	void Start()
+    public void Destroyed()
     {
-        time = Random.Range(0f, 5f);
-        angle = Random.Range(0f, 360f);
-        initialPosition = transform.position;
-    }
-
-    void Update()
-    {
-        time += Time.deltaTime * rate;
-        angle += Time.deltaTime * spinRate;
-
-        Vector3 offset = Vector3.up * Mathf.Sin(time) * amplitude;
-        transform.position = initialPosition + offset;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        RollerGameManager.Instance.Score += points;
     }
 }
---
+++ Assets/RollerBall/Scripts/RollerHealthPickup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerHealthPickup : RollerPickup, IDestructable
+{
+	[SerializeField] float health;
+
+	public void Destroyed()
+	{
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+		go.GetComponent<Health>().health += health;
+	}
+}
